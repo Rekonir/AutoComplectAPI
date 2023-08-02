5e4f8ed787f2d678974e18faa2dd8b77f39d1875
Rekonir <zhed-nikita@mail.ru>
2023-08-02 15:33:30 +0300

    add veiw Cart

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [password, setPassword] = useState("fbk12345fbk");
   const [number, setNumber] = useState("MB831042");
   const [articles, setArticles] = useState([]);
+  const [cart, setCart] = useState([]);
 
   const fetchData = () => {
     setArticles([]);
@@ -21,21 +22,63 @@ function App() {
       .then((data) => {
         console.log(data), setArticles(data);
       })
-      .catch((err) => setArticles([err]));
+      .catch((err) => console.log(err), setArticles([]));
   };
   const addToCart = (priceId) => {
     const id = priceId;
-    const url = `https://www.japarts.ru/?id=ws;action=addtobasket2;login=${login};pass=${password};priceid=${id};`;
-    console.log(id);
+    const url = `https://www.japarts.ru/?id=ws;action=addtobasket2;login=${login};pass=${password};priceid=${id};quantity=10;clientprice=100;comment=test;`;
     axios({
       method: "get",
       url: url,
       responseType: "json",
     })
       .then((data) => data.data)
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data);
+      })
       .catch((err) => console.log(err));
   };
+  const veiwCart = () => {
+    const url = `https://www.japarts.ru/?id=ws;action=showbasket;login=${login};pass=${password}`;
+
+    axios({
+      method: "get",
+      url: url,
+      responseType: "json",
+    })
+      .then((data) => data.data)
+      .then((data) => {
+        console.log(data), setCart(data);
+      })
+      .catch((err) => console.log(err), setCart([]));
+  };
+
+  // const unifyingCart = (cart) => {
+  //   const uniqueCart = cart
+  //   uniqueCart
+  //     .forEach((position) => {
+  //       position.quantity = Number(position.quantity);
+  //     })
+
+  //   // const uniqueCart = cart.filter((position, index, self) => {
+  //   //   // Ищем первое вхождение позиции в массиве
+  //   //   const firstIndex = self.findIndex(
+  //   //     (item) => item.detailnum === position.detailnum
+  //   //   );
+  //   //   // Возвращаем только позиции, у которых индекс равен первому вхождению
+  //   //   return index === firstIndex;
+  //   // });
+  //   for (let i = 0; i < uniqueCart.length; i++) {
+  //     for (let j = i; j < uniqueCart.length; j++) {
+  //       if (uniqueCart[i].detailnum === uniqueCart[j].detailnum) {
+  //         uniqueCart[i].quantity += uniqueCart[j].quantity;
+  //         uniqueCart.splice(j, 1);
+  //       }
+  //     }
+  //   }
+  //   setCart(uniqueCart);
+  //   // console.log(cart);
+  // };
 
   return (
     <>
@@ -57,36 +100,64 @@ function App() {
         value={number}
         onChange={(e) => setNumber(e.target.value)}
       />
-      <button onClick={fetchData}>Search</button>
+      <button onClick={fetchData}>Поиск</button>
+      <button onClick={veiwCart}> Показать корзину</button>
+      <div className="Catalog">
+        <div className="cart">
+          {cart.length !== 0 &&
+            cart.map((position, index) => (
+              <div className="positionBox" key={position.basketid}>
+                <p>№ {index + 1}</p>
+                <p className="makename">
+                  Производитель:
+                  {position.makename ? position.makename : "Нет информации"}
+                </p>
+                <p className="detailname">
+                  Наименование:
+                  {position.detailname ? position.detailname : "Нет информации"}
+                </p>
+                <p className="pricerur">
+                  Стоимость за шт:{" "}
+                  {position.pricerur ? position.pricerur : "Нет информации"} ₽
+                </p>
+                <p className="quantity">В корзине: {position.quantity} шт.</p>
+                <p className="time">
+                  Доставим за: {position.time} дней из {position.country}
+                </p>
+              </div>
+            ))}
+        </div>
 
-      {articles.length !== 0 &&
-        articles.map((article) =>
-          article.error === "NO RESULTS FOUND" ? (
-            <p key={"err"}>Ничего не найдено </p>
-          ) : (
-            <div className="article" key={article.priceid}>
-              <div className="makename">
-                Производитель:{" "}
-                {article.makename ? article.makename : "Undefined"}
-              </div>
-              <div className="detailname">
-                Наименование:{" "}
-                {article.detailname ? article.detailname : "Undefined"}
-              </div>
-              <div className="pricerur">
-                Стоимость: {article.pricerur ? article.pricerur : "Undefined"} ₽
-              </div>
-              <div className="quantity">
-                На складе осталось: {article.quantity ? article.quantity : "0"}{" "}
-                шт.
-              </div>
-              <button onClick={() => addToCart(article.priceid)}>
-                {" "}
-                Заказать{" "}
-              </button>
-            </div>
-          )
-        )}
+        <div className="search">
+          {articles.length !== 0 &&
+            articles.map((article) =>
+              article.error === "NO RESULTS FOUND" ? (
+                <p key={"err"}>Ничего не найдено </p>
+              ) : (
+                <div className="article" key={article.priceid}>
+                  <div className="makename">
+                    Производитель:
+                    {article.makename ? article.makename : "Нет информации"}
+                  </div>
+                  <div className="detailname">
+                    Наименование:
+                    {article.detailname ? article.detailname : "Нет информации"}
+                  </div>
+                  <div className="pricerur">
+                    Стоимость за шт:{" "}
+                    {article.pricerur ? article.pricerur : "Нет информации"} ₽
+                  </div>
+                  <div className="quantity">
+                    На складе: {article.quantity ? article.quantity : "0"} шт.
+                  </div>
+                  <button onClick={() => addToCart(article.priceid)}>
+                    Заказать
+                  </button>
+                </div>
+              )
+            )}
+        </div>
+      </div>
     </>
   );
 }
